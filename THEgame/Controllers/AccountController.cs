30efe11d120177e1c12364895ce4379fd899e57b
Vladimir V. Kudryavtsev <ChinaToster@gmail.com>
2021-02-21 02:26:23 +0300

    1

---
+++ THEgame/Controllers/AccountController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+using THEgame.ViewModels; // пространство имен моделей RegisterModel и LoginModel
+using THEgame.Models; // пространство имен UserContext и класса User
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+
+namespace THEgame.Controllers
+{
+    public class AccountController : Controller
+    {
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+    }
+}
