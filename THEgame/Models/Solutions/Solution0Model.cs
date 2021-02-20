5418b63d2b6edd0c755010f33b94a4cb4cb17044
Vladimir V. Kudryavtsev <ChinaToster@gmail.com>
2021-02-21 00:13:21 +0300

    1

---
+++ THEgame/Models/Solutions/Solution0Model.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace THEgame.Models.Solutions
+{
+    public class Solution0Model
+    {
+        public int RandNumber { get; set; }
+        public IndexModel Imodel { get; set; }
+    }
+}
---
+++ THEgame/Models/Solutions/SolutionsModel.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace THEgame.Models.Solutions
+{
+    public class SolutionsModel
+    {
+        public Solution0Model Sol0 { get; set; }
+    }
+}
